a8e6aa0dd10aa087e1d7b605dbd462be19a42aec
Sven Axelsson <sven@axelsson.name>
2013-01-11 11:09:08 +0100

    Added Heroku info

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "PlayFramework-Tutorial"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "PlayFramework-Tutorial"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here
+  )
 
 }
