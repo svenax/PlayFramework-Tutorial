c8791e9220840d65911e04859548112374010abb
Sven Axelsson <sven@axelsson.name>
2013-01-11 10:32:19 +0100

    Dummy actions added

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,13 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok("views.html.index("Your new application is ready.")")
+    Redirect(routes.Application.tasks)
   }
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 
 }
